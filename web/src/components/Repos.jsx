d1e8706e892274ada56c4cdcdd6354b9a63ec4f2
Ryan <ryan.maceachern@gmail.com>
2022-06-06 12:04:31 -0700

    create basic table with headers for repositories

--- web/src/components/Repos.jsx
+++ web/src/components/Repos.jsx
@@ -1,28 +1,45 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  Paper,
+} from '@mui/material';
 
 const Repos = () => {
-  const [repos, setRepos] = useState();
+  const [repos, setRepos] = useState([]);
 
   useEffect(() => {
     axios
       .get(`http://localhost:4000/repos`)
       .then((res) => {
-        console.log('Response:', res.data);
-        setRepos(...res);
+        // console.log('Response:', res.data);
+        setRepos(...res.data);
+        console.log('Repos:', repos);
       })
       .catch((err) => {
         console.log('Error:', err);
       });
-    return () => {
-      // cleanup
-    };
-  }, []);
+  }, [repos]);
 
   return (
     <>
       <h1>Repos!</h1>
-      <TableContainer></TableContainer>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Repository</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Language</TableCell>
+              <TableCell align="right">Forks</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </>
   );
 };
